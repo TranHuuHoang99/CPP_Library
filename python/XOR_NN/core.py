e385c64d080fee720e3369235345484749a60507
Hoang Tran <thhoang08091999@gmail.com>
2023-02-23 00:42:18 +0700

    Adjusted XOR NN

--- python/XOR_NN/core.py
+++ python/XOR_NN/core.py
@@ -14,6 +14,9 @@ class Brain:
     def loadparam(self, NeuralNetwork) -> sequence:
         index_reversed = 0
 
+        print(self.weight_linear, "\n")
+        print(self.bias_linear)
+
         for i in reversed(range(NeuralNetwork.model.linear.__len__())):
             for j in range(NeuralNetwork.model.linear[i].out_features):
                 for k in range(NeuralNetwork.model.linear[i].in_features):
@@ -21,7 +24,7 @@ class Brain:
                 NeuralNetwork.model.linear[i].bias = self.bias_linear[index_reversed][j]
             index_reversed += 1
 
-        index = 1
+        index = NeuralNetwork.model.linear.__len__() - 1
         for i in range(NeuralNetwork.model.fc.__len__()):
             if NeuralNetwork.model.fc[i].name == "linear":
                 NeuralNetwork.model.fc[i].weight = self.weight_linear[index]
@@ -33,57 +36,45 @@ class Brain:
     def predict(self, input_features, NeuralNetwork) -> np.float64:
         nn:sequence = self.loadparam(NeuralNetwork=NeuralNetwork)
         nn.feedforward(input_features)
-        accuracy = nn.features[0]
+        nn.features[0]
 
-        if accuracy >= 0.6:
+        if nn.features[0] >= 0.6:
             print(1)
-        elif accuracy <= 0.5:
+        elif nn.features[0] <= 0.5:
             print(0)
-        else:
-            print("unknow")
         
-        return accuracy
+        return nn.features[0]
 
 class core:
     def __init__(self) -> None:
         self.model: sequence = ...
         self.brain = Brain()
-
-    def reset_weight(self):
-        for i in reversed(range(self.model.linear.__len__())):
-            self.model.linear[i].delta_weight = np.zeros([self.model.linear[i].out_features, self.model.linear[i].in_features], dtype=np.float64)
-            self.model.linear[i].delta_bias = np.zeros([self.model.linear[i].out_features], dtype=np.float64)
+        self.epoch = 0
     
     def back_propagation(self, learning_rate, lable):
         if self.model == Ellipsis:
             return
+        self.epoch += 1
 
-        self.reset_weight()
+        for i in reversed(range(self.model.linear.__len__())):
+            for j in range(self.model.linear[i].out_features):
+                for k in range(self.model.linear[i].in_features):
+                    self.model.linear[i].delta_weight[j,k] += self.delta_weight(label=lable, i=i, j=j, k=k)
+                    self.model.linear[i].delta_weight[j,k] *= (1/self.epoch)
+                    self.model.linear[i].weight[j,k] -= learning_rate * self.model.linear[i].delta_weight[j,k]
 
-        delta_bias = 0
+                self.model.linear[i].delta_bias[j] += self.delta_bias(label=lable, i=i, j=j, k=0)
+                self.model.linear[i].delta_bias[j] *= (1/self.epoch)
+                self.model.linear[i].bias[j] -= learning_rate * self.model.linear[i].delta_bias[j]
 
+    def save(self, path):
         self.brain.weight_linear = []
         self.brain.bias_linear = []
 
         for i in reversed(range(self.model.linear.__len__())):
-            updated_weight = np.zeros([self.model.linear[i].out_features, self.model.linear[i].in_features], dtype=np.float64)
-            updated_bias = np.zeros([self.model.linear[i].out_features], dtype=np.float64)
+            self.brain.weight_linear.append(self.model.linear[i].weight)
+            self.brain.bias_linear.append(self.model.linear[i].bias)
 
-            for j in range(self.model.linear[i].out_features):
-                for k in range(self.model.linear[i].in_features):
-                    self.model.linear[i].delta_weight[j,k] = self.delta_weight(label=lable, i=i, j=j, k=k)
-                    self.model.linear[i].weight[j,k] -= learning_rate * self.model.linear[i].delta_weight[j,k]
-
-                    updated_weight[j,k] = self.model.linear[i].weight[j,k]
-                    delta_bias = self.delta_bias(label=lable, i=i, j=j, k=k)
-                
-                self.model.linear[i].bias[j] -= learning_rate * delta_bias
-                updated_bias[j] = self.model.linear[i].bias[j]
-
-            self.brain.weight_linear.append(updated_weight)
-            self.brain.bias_linear.append(updated_bias)
-
-    def save(self, path):
         pickle.dump(self.brain, open(path, "wb"))
         
     def delta_weight(self, label, i, j, k) -> np.float64:
