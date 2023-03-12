5137223b1e73263684d04db201755413c38c39a2
Hoang Tran <thhoang08091999@gmail.com>
2023-03-13 00:21:45 +0700

    Update NN

--- python/Seeker/model.py
+++ python/Seeker/model.py
@@ -1,70 +1,34 @@
 from lib import *
-import helper as nn
+from helper import *
 
 # model for seeker
 class SeekerNN:
     def __init__(self) -> None:
         rand.seed(2)
-        self.brain = nn.Brain()
-        self.model = nn.sequence(
-            conv=[
-                nn.conv1d(kernel_length=3),
-                nn.conv1d(kernel_length=3),
-                nn.maxpooling1d(kernel_length=2),
-                
-                nn.conv1d(kernel_length=3),
-                nn.conv1d(kernel_length=3),
-                nn.maxpooling1d(kernel_length=2),
+        self.model = sequence()
+        self.model.seed = 1
+        self.model.learning_rate = 0.01
 
-                nn.conv1d(kernel_length=3),
-                nn.conv1d(kernel_length=3),
-                nn.maxpooling1d(kernel_length=2),
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(maxpooling1d(kernel_length=2))
 
-                nn.conv1d(kernel_length=3),
-                nn.conv1d(kernel_length=3),
-                nn.maxpooling1d(kernel_length=2)
-            ],
-            fc= [
-                nn.linear(features_in=54, features_out=120),
-                nn.relu(),
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(maxpooling1d(kernel_length=2))
 
-                nn.linear(features_in=120, features_out=240),
-                nn.relu(),
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(conv1d(kernel_length=3))
+        self.model.add(maxpooling1d(kernel_length=2))
 
-                nn.linear(features_in=240, features_out=2),
-                nn.relu()
-            ]
-        )
+        self.model.add(linear(features_in=111, features_out=50))
+        self.model.add(relu(features_in=50))
+
+        self.model.add(linear(features_in=50, features_out=25))
+        self.model.add(relu(25))
+
+        self.model.add(linear(features_in=25, features_out=2))
+        self.model.add(relu(features_in=2))
 
     def save(self, path):
-        self.brain.weight_linear = []
-        self.brain.bias_linear = []
-        for i in reversed(range(self.model.linear.__len__())):
-            self.brain.weight_linear.append(self.model.linear[i].weight)
-            self.brain.bias_linear.append(self.model.linear[i].bias)
-        pickle.dump(self.brain, open(path, "wb"))
-
-class XORNN:
-    def __init__(self) -> None:
-        rand.seed(2)
-        self.brain = nn.Brain()
-        self.model = nn.sequence(
-            fc= [
-                nn.linear(features_in=2, features_out=16),
-                nn.relu(),
-
-                nn.linear(features_in=16, features_out=32),
-                nn.relu(),
-
-                nn.linear(features_in=32, features_out=1),
-                nn.relu()
-            ]
-        )
-
-    def save(self, path):
-        self.brain.weight_linear = []
-        self.brain.bias_linear = []
-        for i in reversed(range(self.model.linear.__len__())):
-            self.brain.weight_linear.append(self.model.linear[i].weight)
-            self.brain.bias_linear.append(self.model.linear[i].bias)
-        pickle.dump(self.brain, open(path, "wb"))
+        self.model.save(path)
--- python/Seeker/train.py
+++ python/Seeker/train.py
@@ -1,5 +1,5 @@
 from lib import *
-from model import SeekerNN, XORNN
+from model import SeekerNN
 
 LABEL = 'Category'
 DATA = 'Message'
@@ -12,13 +12,12 @@ def make_data(data_path):
     excel_file = pandas.read_csv(data_path)
     data_arr = []
     label_arr = []
-    
-    max_length = 100
+    max_length = 910
 
     sum_ascii = 0
     get_word_total_ascii_int = []
     split_word = []
-    for i in range(int(excel_file[LABEL].__len__())):
+    for i in range(int(excel_file[LABEL].__len__() / 3)):
         label_arr.append(excel_file[LABEL][i])
         split_word = []
         split_word = excel_file[DATA][i].split()
@@ -31,10 +30,10 @@ def make_data(data_path):
         data_arr.append(get_word_total_ascii_int)
 
     for i in range(data_arr.__len__()):
-        if data_arr[i].__len__() > 910:
+        if data_arr[i].__len__() > max_length:
             data_arr[i][:] = data_arr[i][: (data_arr[i].__len__() - (data_arr[i].__len__() - 100))]
-        elif data_arr[i].__len__() < 910:
-            for new in range(int(910) - int(data_arr[i].__len__())):
+        elif data_arr[i].__len__() < max_length:
+            for new in range(int(max_length) - int(data_arr[i].__len__())):
                 data_arr[i].append(int(32)) #32 equal to " " (space)
         else:
             pass
@@ -48,73 +47,28 @@ def make_lable(input):
         return [1,0]
     return [0,1]
 
-def relu(input: float):
-    if (input < 0).any():
-        return -input
-    return input
-
-def cal_loss(pred, label):
-    avg_pred = 0
-    for i in range(label.__len__()):
-        avg_pred += (relu((pred[i] - label[i])))
-    return avg_pred / label.__len__()
-
-def seeker_fit():
+def fit():
     model = SeekerNN()
     root_path = os.path.abspath(os.path.dirname(__file__))
     root_brain = root_path + '\\seeker_nn.brain'
     root_excel_path = root_path + '\\raw_data\spam_text.csv'
     data = make_data(root_excel_path)
-    learning_rate = 0.4
 
-    for i in range(data[2]):
-        print("LABEL : %a, %s"%(make_lable(data[0][i]), data[0][i]))
-        model.model.forward_prop(data[1][i])
-        loss = cal_loss(pred=model.model.output, label=make_lable(data[0][i]))
-        print("loss : ", loss)
-        model.model.backward_prop(learning_rate=learning_rate, label=make_lable(data[0][i]))
+    for epoch in range(30):
+        for i in range(data[2]):
+            model.model.forward_prop(data[1][i], label=make_lable(data[0][i]))
+            print(model.model.features)
+            print("LOSS IS : ", model.model.loss, " LABEL IS : ", data[0][i])
+            print()
 
+            model.model.backward_prop(label=make_lable(data[0][i]))
+            model.model.gradient_descend()
+            
     model.save(root_brain)
     print(data[0][159])
     predict = pickle.load(open(root_brain, "rb"))
-    print(predict.prediction_value(data[1][159], model))
-
-def atttach_lable(arr) -> np.float64:
-    if arr[0] == 0 and arr[1] == 1:
-        return [1]
-    elif arr[0] == 0 and arr[1] == 0:
-        return [0]
-    elif arr[0] == 1 and arr[1] == 1:
-        return [0]
-    else:
-        return [1]
-
-def none_minus(input) -> np.float64:
-        if input < 0:
-            return -input
-        return input
-
-def xor_fit():
-    model = XORNN()
-    root_path = os.path.abspath(os.path.dirname(__file__))
-    root_brain = root_path + '\\xornn.brain'
-    
-    for i in range(100000):
-        arr = [np.float64(rand.randint(0,1)), np.float64(rand.randint(0,1))]
-        model.model.forward_prop(arr)
-        print("INPUT : ", arr[0], " ", arr[1], " LABEL IS: ", atttach_lable(arr=arr))
-        print("accuracy : ", 1 - (none_minus(atttach_lable(arr=arr) - model.model.output[0])), "loss : ", (none_minus(atttach_lable(arr=arr) - model.model.output[0])))
-        print()
-        model.model.backward_prop(0.4, atttach_lable(arr=arr))
-
-    model.save(root_brain)
-
-    print("\n")
-
-    predict = pickle.load(open(root_brain, "rb"))
-    print(predict.prediction_value([0,0], NeuralNetwork=model))
-    
+    predict.set_model(model)
+    predict.predict(data[1][159], make_lable(data[0][159]))
 
 if __name__ == "__main__":
-    # xor_fit()
-    seeker_fit()
+    fit()
