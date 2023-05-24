65a1881fb243bce3d351ceb706e38cdf9f1cdc16
TranHuuHoang99 <Hoang.TranHuu@vn.bosch.com>
2023-05-24 14:16:32 +0700

    24/5/2023

--- python/Seeker/model.py
+++ python/Seeker/model.py
@@ -1,16 +1,18 @@
 from lib import *
-# from helper import *
-from CNN_Module import linear, sequence, softmax
+from helper import *
+# from CNN_Module import linear, sequence, softmax
 
 # model for seeker
 class SeekerNN:
     def __init__(self) -> None:
-        self.model = sequence(seed=4321)
-        self.model.learning_rate = 0.0005
+        self.model = sequence(seed=1234)
+        self.model.learning_rate = 0.01
 
-        self.model.add(linear(features_in=171, features_out=16, activation='relu'))
-        self.model.add(linear(features_in=16, features_out=2, activation='relu'))
-        self.model.add(softmax(2))
+        self.model.add(linear(features_in=171, features_out=342))
+        self.model.add(relu(features_in=342))
+
+        self.model.add(linear(features_in=342, features_out=2))
+        self.model.add(relu(features_in=2))
 
     def save(self, path):
         self.model.save_model(path)
